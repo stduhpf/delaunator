d4e5121019b7c779ad7f8d41a6f5b327ad77f26f
Stéphane du Hamel <stephduh@live.fr>
2022-08-01 02:40:35 +0200

    fix missing mc colors

--- minecraftColors.py
+++ minecraftColors.py
@@ -1,4 +1,4 @@
-from math import floor, pow
+from math import floor, pow, sqrt
 from PIL import Image
 import numpy as np
 
@@ -19,60 +19,79 @@ colors = {
     "CLAY": [164, 168, 184],
     "DIRT": [151, 109, 77],
     "STONE": [112, 112, 112],
+    "WOOD": [143, 119, 72],
     "WATER": [64, 64, 255],
     "QUARTZ": [255, 252, 245],
     "COLOR_ORANGE": [216, 127, 51],
     "COLOR_MAGENTA": [178, 76, 216],
     "COLOR_LIGHT_BLUE": [102, 153, 216],
-    # "COLOR_YELLOW": [229, 229, 51],
-    # "COLOR_LIGHT_GREEN": [127, 204, 25],
-    # "COLOR_PINK": [242, 127, 165],
-    # "COLOR_GRAY": [76, 76, 76],
-    # "COLOR_CYAN": [76, 127, 153],
-    # "COLOR_PURPLE": [127, 63, 178],
-    # "COLOR_BLUE": [51, 76, 178],
-    # "COLOR_BROWN": [102, 76, 51],
-    # "COLOR_GREEN": [102, 127, 51],
-    # "COLOR_RED": [153, 51, 51],
-    # "COLOR_BLACK": [25, 25, 25],
-    # "GOLD": [250, 238, 77],
-    # "DIAMOND": [92, 219, 213],
-    # "LAPIS": [74, 128, 255],
-    # "EMERALD": [0, 217, 58],
-    # "PODZOL": [129, 86, 49],
-    # "NETHER": [112, 2, 0],
-    # "TERRACOTTA_WHITE": [209, 177, 161],
-    # "TERRACOTTA_ORANGE": [159, 82, 36],
-    # "TERRACOTTA_MAGENTA": [149, 87, 108],
-    # "TERRACOTTA_LIGHT_BLUE": [112, 108, 138],
-    # "TERRACOTTA_YELLOW": [186, 133, 36],
-    # "TERRACOTTA_LIGHT_GREEN": [103, 117, 53],
-    # "TERRACOTTA_PINK": [160, 77, 78],
-    # "TERRACOTTA_GRAY": [57, 41, 35],
-    # "TERRACOTTA_LIGHT_GRAY": [135, 107, 98],
-    # "TERRACOTTA_CYAN": [87, 92, 92],
-    # "TERRACOTTA_PURPLE": [122, 73, 88],
-    # "TERRACOTTA_BLUE": [76, 62, 92],
-    # "TERRACOTTA_BROWN": [76, 50, 35],
-    # "TERRACOTTA_GREEN": [76, 82, 42],
-    # "TERRACOTTA_RED": [142, 60, 46],
-    # "TERRACOTTA_BLACK": [37, 22, 16],
-    # "CRIMSON_NYLIUM": [189, 48, 49],
-    # "CRIMSON_STEM": [148, 63, 97],
-    # "CRIMSON_HYPHAE": [92, 25, 29],
-    # "WARPED_NYLIUM": [22, 126, 134],
-    # "WARPED_STEM": [58, 142, 140],
-    # "WARPED_HYPHAE": [86, 44, 62],
-    # "WARPED_WART_BLOCK": [20, 180, 133],
-    # "DEEPSLATE": [100, 100, 100],
-    # "RAW_IRON": [216, 175, 147],
-    # "GLOW_LICHEN": [127, 167, 150],
+    "COLOR_YELLOW": [229, 229, 51],
+    "COLOR_LIGHT_GREEN": [127, 204, 25],
+    "COLOR_PINK": [242, 127, 165],
+    "COLOR_GRAY": [76, 76, 76],
+    "COLOR_LIGHT_GRAY": [153, 153, 153],
+    "COLOR_CYAN": [76, 127, 153],
+    "COLOR_PURPLE": [127, 63, 178],
+    "COLOR_BLUE": [51, 76, 178],
+    "COLOR_BROWN": [102, 76, 51],
+    "COLOR_GREEN": [102, 127, 51],
+    "COLOR_RED": [153, 51, 51],
+    "COLOR_BLACK": [25, 25, 25],
+    "GOLD": [250, 238, 77],
+    "DIAMOND": [92, 219, 213],
+    "LAPIS": [74, 128, 255],
+    "EMERALD": [0, 217, 58],
+    "PODZOL": [129, 86, 49],
+    "NETHER": [112, 2, 0],
+    "TERRACOTTA_WHITE": [209, 177, 161],
+    "TERRACOTTA_ORANGE": [159, 82, 36],
+    "TERRACOTTA_MAGENTA": [149, 87, 108],
+    "TERRACOTTA_LIGHT_BLUE": [112, 108, 138],
+    "TERRACOTTA_YELLOW": [186, 133, 36],
+    "TERRACOTTA_LIGHT_GREEN": [103, 117, 53],
+    "TERRACOTTA_PINK": [160, 77, 78],
+    "TERRACOTTA_GRAY": [57, 41, 35],
+    "TERRACOTTA_LIGHT_GRAY": [135, 107, 98],
+    "TERRACOTTA_CYAN": [87, 92, 92],
+    "TERRACOTTA_PURPLE": [122, 73, 88],
+    "TERRACOTTA_BLUE": [76, 62, 92],
+    "TERRACOTTA_BROWN": [76, 50, 35],
+    "TERRACOTTA_GREEN": [76, 82, 42],
+    "TERRACOTTA_RED": [142, 60, 46],
+    "TERRACOTTA_BLACK": [37, 22, 16],
+    "CRIMSON_NYLIUM": [189, 48, 49],
+    "CRIMSON_STEM": [148, 63, 97],
+    "CRIMSON_HYPHAE": [92, 25, 29],
+    "WARPED_NYLIUM": [22, 126, 134],
+    "WARPED_STEM": [58, 142, 140],
+    "WARPED_HYPHAE": [86, 44, 62],
+    "WARPED_WART_BLOCK": [20, 180, 133],
+    "DEEPSLATE": [100, 100, 100],
+    "RAW_IRON": [216, 175, 147],
+    "GLOW_LICHEN": [127, 167, 150],
 }
 multipliers = [0.71, 0.86, 1.0, 0.53]
 
 multipliers = [0.71, 0.86, 1.0]
 
-multipliers = [0.86]
+# multipliers = [0.86]
+
+# TODO: Use CIELUV for closest color matching
+def closestColor(
+    colorList: list[tuple[str, list[float]]], color: list[float]
+) -> list[float]:
+    col = colorList[0][1]
+    md = 100000.0
+
+    for (_, c) in colorList:
+        dx = color[0] - c[0]
+        dy = color[1] - c[1]
+        dz = color[2] - c[2]
+        d = sqrt(dx * dx + dy * dy + dz * dz)
+        if d < md:
+            col = c
+            md = d
+    return col
 
 
 def bayer(x: int, y: int, level: int) -> float:
@@ -135,14 +154,12 @@ for (i, m) in enumerate(multipliers):
     colorList += [
         (
             c[0] + "|" + str(i),
-            (
-                gammaInv(
-                    [
-                        c[1][0] * m,
-                        c[1][1] * m,
-                        c[1][2] * m,
-                    ]
-                )
+            gammaInv(
+                [
+                    c[1][0] * m,
+                    c[1][1] * m,
+                    c[1][2] * m,
+                ]
             ),
         )
         for c in colors.items()
@@ -152,7 +169,7 @@ for (i, m) in enumerate(multipliers):
 palette = run(colorList)
 
 print("palette generated")
-exit()
+# exit()
 
 # ax = plt.axes(projection="3d")
 # ax.set_box_aspect([1, 1, 1])
@@ -178,6 +195,7 @@ newImage_b = Image.new(image.mode, image.size)
 newImage_w = Image.new(image.mode, image.size)
 newImage_ign = Image.new(image.mode, image.size)
 newImage_bay = Image.new(image.mode, image.size)
+newImage_close = Image.new(image.mode, image.size)
 
 csv = []
 
@@ -192,7 +210,7 @@ for y in range(image.size[1]):
         #         190 * X + 255 * (1.0 - X),
         #     ]
         # )
-        color = gammaInv([p for p in pixels[x, y]])
+        color = gammaInv([float(p) for p in pixels[x, y]])
         # print(f"{pixels[x, y]} => {color}")
         p = Point.make(color[0], color[1], color[2])
         t = findLocalTetra(p, palette)
@@ -271,6 +289,9 @@ for y in range(image.size[1]):
         else:
             c = gamma(t[0].vertices[0].val.tolist())
         newImage_bay.putpixel((x, y), (int(c[0]), int(c[1]), int(c[2]), 255))
+
+        c = gamma(closestColor(colorList, color))
+        newImage_close.putpixel((x, y), (int(c[0]), int(c[1]), int(c[2]), 255))
         # c = gamma((t[0].o + np.matmul(xyz, t[0].M)).tolist())
     # print("\n")
 image.save("scaled_.png")
@@ -278,7 +299,8 @@ newImage_w.save("outw.png")
 newImage_b.save("outb.png")
 newImage_ign.save("outign.png")
 newImage_bay.save("outbay.png")
+newImage_close.save("out_close.png")
 
 print("saved!\n")
 
-print(csv)
+# print(csv)
