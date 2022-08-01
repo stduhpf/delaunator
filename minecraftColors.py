788a3c2a2ccbca66d69ce2bb202ccb95a7d8a5e1
Stéphane du Hamel <stephduh@live.fr>
2022-08-01 16:43:59 +0200

    add option to ignore small weight vetices

--- minecraftColors.py
+++ minecraftColors.py
@@ -8,67 +8,67 @@ import matplotlib.pyplot as plt
 # TODO: gamma correction, color spaces, accelerating structure
 
 colors = {
-    "GRASS": [127, 178, 56],
-    "SAND": [247, 233, 163],
-    "WOOL": [199, 199, 199],
-    "FIRE": [255, 0, 0],
-    "ICE": [160, 160, 255],
-    "METAL": [167, 167, 167],
-    "PLANT": [0, 124, 0],
-    "SNOW": [255, 255, 255],
-    "CLAY": [164, 168, 184],
-    "DIRT": [151, 109, 77],
-    "STONE": [112, 112, 112],
-    "WOOD": [143, 119, 72],
-    "WATER": [64, 64, 255],
-    "QUARTZ": [255, 252, 245],
-    "COLOR_ORANGE": [216, 127, 51],
-    "COLOR_MAGENTA": [178, 76, 216],
-    "COLOR_LIGHT_BLUE": [102, 153, 216],
-    "COLOR_YELLOW": [229, 229, 51],
-    "COLOR_LIGHT_GREEN": [127, 204, 25],
-    "COLOR_PINK": [242, 127, 165],
-    "COLOR_GRAY": [76, 76, 76],
-    "COLOR_LIGHT_GRAY": [153, 153, 153],
-    "COLOR_CYAN": [76, 127, 153],
-    "COLOR_PURPLE": [127, 63, 178],
-    "COLOR_BLUE": [51, 76, 178],
-    "COLOR_BROWN": [102, 76, 51],
-    "COLOR_GREEN": [102, 127, 51],
-    "COLOR_RED": [153, 51, 51],
-    "COLOR_BLACK": [25, 25, 25],
-    "GOLD": [250, 238, 77],
-    "DIAMOND": [92, 219, 213],
-    "LAPIS": [74, 128, 255],
-    "EMERALD": [0, 217, 58],
-    "PODZOL": [129, 86, 49],
-    "NETHER": [112, 2, 0],
-    "TERRACOTTA_WHITE": [209, 177, 161],
-    "TERRACOTTA_ORANGE": [159, 82, 36],
-    "TERRACOTTA_MAGENTA": [149, 87, 108],
-    "TERRACOTTA_LIGHT_BLUE": [112, 108, 138],
-    "TERRACOTTA_YELLOW": [186, 133, 36],
-    "TERRACOTTA_LIGHT_GREEN": [103, 117, 53],
-    "TERRACOTTA_PINK": [160, 77, 78],
-    "TERRACOTTA_GRAY": [57, 41, 35],
-    "TERRACOTTA_LIGHT_GRAY": [135, 107, 98],
-    "TERRACOTTA_CYAN": [87, 92, 92],
-    "TERRACOTTA_PURPLE": [122, 73, 88],
-    "TERRACOTTA_BLUE": [76, 62, 92],
-    "TERRACOTTA_BROWN": [76, 50, 35],
-    "TERRACOTTA_GREEN": [76, 82, 42],
-    "TERRACOTTA_RED": [142, 60, 46],
-    "TERRACOTTA_BLACK": [37, 22, 16],
-    "CRIMSON_NYLIUM": [189, 48, 49],
-    "CRIMSON_STEM": [148, 63, 97],
-    "CRIMSON_HYPHAE": [92, 25, 29],
-    "WARPED_NYLIUM": [22, 126, 134],
-    "WARPED_STEM": [58, 142, 140],
-    "WARPED_HYPHAE": [86, 44, 62],
-    "WARPED_WART_BLOCK": [20, 180, 133],
-    "DEEPSLATE": [100, 100, 100],
-    "RAW_IRON": [216, 175, 147],
-    "GLOW_LICHEN": [127, 167, 150],
+    "GRASS":                  [127, 178, 56 ],
+    "SAND":                   [247, 233, 163],
+    "WOOL":                   [199, 199, 199],
+    "FIRE":                   [255, 0  , 0  ],
+    "ICE":                    [160, 160, 255],
+    "METAL":                  [167, 167, 167],
+    "PLANT":                  [0  , 124, 0  ],
+    "SNOW":                   [255, 255, 255],
+    "CLAY":                   [164, 168, 184],
+    "DIRT":                   [151, 109, 77 ],
+    "STONE":                  [112, 112, 112],
+    "WOOD":                   [143, 119, 72 ],
+    "WATER":                  [64 , 64 , 255],
+    "QUARTZ":                 [255, 252, 245],
+    "COLOR_ORANGE":           [216, 127, 51 ],
+    "COLOR_MAGENTA":          [178, 76 , 216],
+    "COLOR_LIGHT_BLUE":       [102, 153, 216],
+    "COLOR_YELLOW":           [229, 229, 51 ],
+    "COLOR_LIGHT_GREEN":      [127, 204, 25 ],
+    "COLOR_PINK":             [242, 127, 165],
+    "COLOR_GRAY":             [76 , 76 , 76 ],
+    "COLOR_LIGHT_GRAY":       [153, 153, 153],
+    "COLOR_CYAN":             [76 , 127, 153],
+    "COLOR_PURPLE":           [127, 63 , 178],
+    "COLOR_BLUE":             [51 , 76 , 178],
+    "COLOR_BROWN":            [102, 76 , 51 ],
+    "COLOR_GREEN":            [102, 127, 51 ],
+    "COLOR_RED":              [153, 51 , 51 ],
+    "COLOR_BLACK":            [25 , 25 , 25 ],
+    "GOLD":                   [250, 238, 77 ],
+    "DIAMOND":                [92 , 219, 213],
+    "LAPIS":                  [74 , 128, 255],
+    "EMERALD":                [0  , 217, 58 ],
+    "PODZOL":                 [129, 86 , 49 ],
+    "NETHER":                 [112, 2  , 0  ],
+    "TERRACOTTA_WHITE":       [209, 177, 161],
+    "TERRACOTTA_ORANGE":      [159, 82 , 36 ],
+    "TERRACOTTA_MAGENTA":     [149, 87 , 108],
+    "TERRACOTTA_LIGHT_BLUE":  [112, 108, 138],
+    "TERRACOTTA_YELLOW":      [186, 133, 36 ],
+    "TERRACOTTA_LIGHT_GREEN": [103, 117, 53 ],
+    "TERRACOTTA_PINK":        [160, 77 , 78 ],
+    "TERRACOTTA_GRAY":        [57 , 41 , 35 ],
+    "TERRACOTTA_LIGHT_GRAY":  [135, 107, 98 ],
+    "TERRACOTTA_CYAN":        [87 , 92 , 92 ],
+    "TERRACOTTA_PURPLE":      [122, 73 , 88 ],
+    "TERRACOTTA_BLUE":        [76 , 62 , 92 ],
+    "TERRACOTTA_BROWN":       [76 , 50 , 35 ],
+    "TERRACOTTA_GREEN":       [76 , 82 , 42 ],
+    "TERRACOTTA_RED":         [142, 60 , 46 ],
+    "TERRACOTTA_BLACK":       [37 , 22 , 16 ],
+    "CRIMSON_NYLIUM":         [189, 48 , 49 ],
+    "CRIMSON_STEM":           [148, 63 , 97 ],
+    "CRIMSON_HYPHAE":         [92 , 25 , 29 ],
+    "WARPED_NYLIUM":          [22 , 126, 134],
+    "WARPED_STEM":            [58 , 142, 140],
+    "WARPED_HYPHAE":          [86 , 44 , 62 ],
+    "WARPED_WART_BLOCK":      [20 , 180, 133],
+    "DEEPSLATE":              [100, 100, 100],
+    "RAW_IRON":               [216, 175, 147],
+    "GLOW_LICHEN":            [127, 167, 150],
 }
 multipliers = [0.71, 0.86, 1.0, 0.53]
 
@@ -96,15 +96,19 @@ def closestColor(
 
 def bayer(x: int, y: int, level: int) -> float:
     if level <= 0:
-        return 0.5
+        return 0.
     if level == 1:
+        return fract(y*y*.75+x*.5)
+    if(level < 3):
+        return bayer(int(x/2), int(y/2), level-1) / 4 + bayer(x, y, 1)
+    if level == 3:
         v = bayer.size[0] * bayer.size[1]
         x = x % bayer.size[0]
         y = y % bayer.size[1]
         return (bayer.data[x, y]) / 254.0 * (v - 1) / v
-    return bayer(int(x / bayer.size[0]), int(y / bayer.size[1]), level - 1) / (
+    return bayer(int(x / bayer.size[0]), int(y / bayer.size[1]), level - 3) / (
         bayer.size[0] * bayer.size[1]
-    ) + bayer(x, y, 1)
+    ) + bayer(x, y, 3)
 
 
 def bayer_d(x: int, y: int, level: int) -> float:
@@ -119,9 +123,9 @@ def gammaInv(col: list[float]) -> list[float]:
 
 def rgb2ycocg(col: list[float]) -> list[float]:
     return [
-        0.25 * col[0] + 0.5 * col[1] + 0.25 * col[2],
-        0.5 * col[0] - 0.5 * col[2],
-        -0.25 * col[0] + 0.5 * col[1] - 0.25 * col[2],
+         0.25  * col[0] + 0.5 * col[1] + 0.25 * col[2],
+         0.5   * col[0]                - 0.5  * col[2],
+        -0.25  * col[0] + 0.5 * col[1] - 0.25 * col[2],
     ]
 
 
@@ -133,7 +137,7 @@ def gamma(col: list[float]) -> list[float]:
 def ycocg2rgb(col: list[float]) -> list[float]:
     return [
         col[0] + col[1] - col[2],
-        col[0] + col[2],
+        col[0]          + col[2],
         col[0] - col[1] - col[2],
     ]
 
@@ -146,7 +150,7 @@ def ign(x, y):
     return fract(52.9829189 * fract(0.06711056 * float(x) + 0.00583715 * float(y)))
 
 
-image = Image.open("tests/malty.png").resize((128, 199))
+image = Image.open("tests/lena_std.tif").resize((128, 128))
 
 
 colorList = []
@@ -191,13 +195,15 @@ bayer.data = bayer8.load()
 newImageData = []
 
 pixels = image.load()
-newImage_b = Image.new(image.mode, image.size)
-newImage_w = Image.new(image.mode, image.size)
-newImage_ign = Image.new(image.mode, image.size)
-newImage_bay = Image.new(image.mode, image.size)
+newImage_b     = Image.new(image.mode, image.size)
+newImage_w     = Image.new(image.mode, image.size)
+newImage_ign   = Image.new(image.mode, image.size)
+newImage_bay   = Image.new(image.mode, image.size)
 newImage_close = Image.new(image.mode, image.size)
 
 csv = []
+
+offset = 0
 
 for y in range(image.size[1]):
     csv.append([])
@@ -220,8 +226,32 @@ for y in range(image.size[1]):
             continue
         c = []
         xyz = t[1]
-        # r = np.random.random()
-        r = blueNoise[x % bns[0], y % bns[1]][0] / 255.0
+        r = 1.-xyz[0]-xyz[1]-xyz[2]
+        while(min(xyz[0],xyz[1],xyz[2],r)<offset):
+            m = min(xyz[0],xyz[1],xyz[2],r)
+            d = 1 - m
+            if(m==xyz[0]):
+                xyz[1]+=xyz[0]*xyz[1]/d
+                xyz[2]+=xyz[0]*xyz[2]/d
+                r     +=xyz[0]*r     /d
+                xyz[0] = 2
+            elif(m==xyz[1]):
+                xyz[0]+=xyz[1]*xyz[0]/d
+                xyz[2]+=xyz[1]*xyz[2]/d
+                r     +=xyz[1]*r     /d
+                xyz[1] = 2
+            elif(m==xyz[2]):
+                xyz[0]+=xyz[2]*xyz[0]/d
+                xyz[1]+=xyz[2]*xyz[1]/d
+                r     +=xyz[2]*r     /d
+                xyz[2] = 2
+            else:
+                xyz[0]+=r     *xyz[0]/d
+                xyz[1]+=r     *xyz[1]/d
+                xyz[2]+=r     *xyz[2]/d
+                r=2
+        xyz = [0 if v > 1 else v for v in xyz]
+        r = (blueNoise[x % bns[0], y % bns[1]][0] + 0.5) / 256.0
         i = 0
         if r > xyz[0]:
             r -= xyz[0]
@@ -275,7 +305,7 @@ for y in range(image.size[1]):
         name = t[0].vertices[i].name
         csv[y] += name.split("|")
 
-        r = bayer_d(x, y, 2)
+        r = bayer_d(x, y, 7)
         if r > xyz[0]:
             r -= xyz[0]
             if r > xyz[1]:
@@ -294,11 +324,11 @@ for y in range(image.size[1]):
         newImage_close.putpixel((x, y), (int(c[0]), int(c[1]), int(c[2]), 255))
         # c = gamma((t[0].o + np.matmul(xyz, t[0].M)).tolist())
     # print("\n")
-image.save("scaled_.png")
-newImage_w.save("outw.png")
-newImage_b.save("outb.png")
-newImage_ign.save("outign.png")
-newImage_bay.save("outbay.png")
+image.save(         "scaled_.png"  )
+newImage_w.save(    "outw.png"     )
+newImage_b.save(    "outb.png"     )
+newImage_ign.save(  "outign.png"   )
+newImage_bay.save(  "outbay.png"   )
 newImage_close.save("out_close.png")
 
 print("saved!\n")
