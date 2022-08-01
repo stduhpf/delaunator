5882777cb98625d47b63045a5ee4b1ef9a79f4ef
Stéphane du Hamel <stephduh@live.fr>
2022-08-02 00:58:42 +0200

    default pic

--- minecraftColors.py
+++ minecraftColors.py
@@ -191,7 +191,7 @@ def ign(x, y):
     return fract(52.9829189 * fract(0.06711056 * float(x) + 0.00583715 * float(y)))
 
 
-image = Image.open("tests/jojokerker.PNG").resize((128, 128))
+image = Image.open("tests/lena_std.tif").resize((128, 128))
 
 
 colorList = []
